679238bc59952edc0cbbd736a1448a9c3a1241e2
kishore Jr <36214175+kishoreravi24@users.noreply.github.com>
2020-01-09 07:05:30 +0530

    Add files via upload

---
+++ Heap/Max_Heap.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+int main(){
+    std::priority_queue<int> pq;
+    int n,x;
+    std::cin>>n;
+    while(n-->0){
+        std::cin>>x;
+        pq.push(x);
+    }
+    //Max Heap;
+    while(!pq.empty()){
+        std::cout<<pq.top()<<" ";
+        pq.pop();
+    }
+}
